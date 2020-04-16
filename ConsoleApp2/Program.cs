ee23fbb1cd137942bfb70bc826c1a35775223bcd
Ole Nielsen (ONE) <ONE@kmd.dk>
2020-04-16 16:03:30 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine(i);
+                int b;
+
+            }
+        }
+    }
+}
